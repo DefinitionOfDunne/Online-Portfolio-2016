3c221d332108b814769f5b3c4313df0cdc1e4343
Dylan Dunne <dylanadunne@gmail.com>
2016-10-29 09:15:08 -0400

    added animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,8 @@
 $(function() {
-    $(".content-circle").mouseenter(function(event) {
-        $(this).addClass("animated shake");
+    $(".project-img").mouseenter(function(event) {
+        $(this).addClass("animated pulse");
     });
-    $(".circle-wrapper").on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(event) {
-        $(this).removeClass("animated shake");
+    $(".project-img").mouseleave(function(event) {
+        $(this).removeClass("animated pulse");
     });
-});
-
-
-$('#project-zero').popover({
-    title: "From 1990 - 2015:",
-    html: true,
-    trigger: "hover",
-    content: function() {
-        return $("#popoverContentFirst").clone();
-    }
-});
-
-$('#project-one').popover({
-    title: "Romance With Russ Comment Generator",
-    html: true,
-    trigger: "hover",
-    content: function() {
-        return $("#popoverContentSecond").clone();
-    }
-});
-
-$('#project-two').popover({
-    title: "PodFan: A Resource For Pod Lovers",
-    html: true,
-    trigger: "hover",
-    content: function() {
-        return $("#popoverContentThird").clone();
-    }
-});
-
-$('#project-three').popover({
-    title: "Official Evan Lian Fan Page",
-    html: true,
-    trigger: "hover",
-    content: function() {
-        return $("#popoverContentFourth").clone();
-    }
-});
-
-$('#project-four').popover({
-    title: "Universally Rendered Blogging Application",
-    html: true,
-    trigger: "hover",
-    content: function() {
-        return $("#popoverContentFifth").clone();
-    }
 });
